c9c4936f33c6b896b3d60ab8a2258a2945620aa2
Meraj <buildwithmeraj.com>
2025-11-10 15:17:35 +0600

    configured mongodb access

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://buildwithmeraj_db_user:3xGWeKrSs1HYGmEv@cityfixdata.krf4nxz.mongodb.net/?appName=cityfixData";
+const client = new MongoClient(uri, {
+  tls: true,
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from backend!");
 });
+
+async function run() {
+  try {
+    console.log("Connected to MongoDB Atlas!");
+
+    const db = client.db("categoriesDB");
+    const categoriesCollection = db.collection("categories");
+
+    app.get("/categories", async (req, res) => {
+      const categories = await categoriesCollection.find({}).toArray();
+      res.json(categories);
+    });
+
+    app.post("/categories", async (req, res) => {
+      const newCategory = req.body;
+      const result = await categoriesCollection.insertOne(newCategory);
+      res.status(201).json(result);
+    });
+
+    app.listen(port, () =>
+      console.log(`Server running at http://localhost:${port}`)
+    );
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+  }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
